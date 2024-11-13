1599c7a1c1d956d9eb862634cf912e946aef8d68
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-11-13 06:38:14 -0500

    basic express setup

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+require('dotenv/config');
+const path = require('node:path');
+const express = require('express');
 
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const assetsPath = path.join(__dirname, 'public');
+app.use(express.static(assetsPath));
