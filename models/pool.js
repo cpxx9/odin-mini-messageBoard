9a574beb46f52f75dd6bb1cb1490950280c50505
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-11-18 12:20:58 -0500

    added commented out dev db string

--- models/pool.js
+++ models/pool.js
@@ -4,3 +4,6 @@ const { Pool } = require('pg');
 module.exports = new Pool({
   connectionString: process.env.DB_URL,
 });
+
+// DEV string
+// `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
