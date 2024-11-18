80c7b2c2babec4ee5710f8b375793ccc95124d39
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-11-18 12:10:20 -0500

    errors

--- controllers/newController.js
+++ controllers/newController.js
@@ -1,14 +1,42 @@
+const { body, validationResult } = require('express-validator');
 const { insertMessage } = require('../models/queries');
 
-const postMessage = async (req, res) => {
-  const newMessage = {
-    text: req.body.messageText,
-    username: req.body.authorName,
-  };
+const alphaNumErr = 'must only contain letters and numbers';
+const usernameLengthErr = 'must be between 1 and 20 characters';
+const lengthErr = 'must be no more than 250 characters';
 
-  await insertMessage(newMessage);
+const validateMessage = [
+  body('authorName')
+    .trim()
+    .isAlphanumeric()
+    .withMessage(`Username: ${alphaNumErr}`)
+    .isLength({ min: 1, max: 20 })
+    .withMessage(`Username: ${usernameLengthErr}`),
+  body('messageText')
+    .trim()
+    .isLength({ max: 250 })
+    .withMessage(`Message: ${lengthErr}`),
+];
 
-  res.redirect('/');
-};
+const postMessage = [
+  validateMessage,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render('form', {
+        title: 'New message',
+        errors: errors.array(),
+      });
+    }
+    const newMessage = {
+      text: req.body.messageText,
+      username: req.body.authorName,
+    };
+
+    await insertMessage(newMessage);
+
+    res.redirect('/');
+  },
+];
 
 module.exports = { postMessage };
